fb57acdc08b169b667427129776a91025fd63da0
Tantan Fu <luckytantanfu@gmail.com>
2019-06-05 20:43:50 +0800

    define siri

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function siri(callback) {
+  callback.next(1);
+  setTimeout(() => {
+    callback.next(2);
+    callback.complete();
+  }, 3000);
+}
+
 const callback = {
   next(x)    { console.log("☕️ Time to drink " + x); },
   error(err) { console.log("❌ Oops " + err); },
